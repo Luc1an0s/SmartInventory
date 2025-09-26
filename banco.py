0a32e89c3b05726e4028b6314e0200847a58ebb6
Luc1an0s <lucianomaciel53@gmail.com>
2025-09-26 15:58:41 -0400

    Update banco.py

--- banco.py
+++ banco.py
@@ -4,9 +4,17 @@ import sqlite3
 
 df = pd.read_excel("estoque.xlsx")
 
-
 conn = sqlite3.connect("estoque.db")
 
 df.to_sql("produtos", conn, if_exists="replace", index=False)
 
 conn.close()
+
+vendas_df = pd.read_excel("vendas.xlsx")
+
+conn = sqlite3.connect("estoque.db")
+
+vendas_df.to_sql("vendas", conn, if_exists="replace", index=False)
+
+conn.close()
+print("Tabela 'vendas' inserida no banco de dados!")
